3d1aa9c51fe467ececd226220b5fee632caf3ec8
BokilaLin <eet02948@gmail.com>
2019-07-04 14:26:33 +0800

    Fix typo

--- firebird/operations.py
+++ firebird/operations.py
@@ -1,6 +1,6 @@
 import uuid
 import datetime
-from decimal import Decimial
+from decimal import Decimal
 
 from django.conf import settings
 from django.db.backends.base.operations import BaseDatabaseOperations
